c8d1f574c83b12134f0fde50aa8c1263b44ccfb3
Mr. Fluffy <git@sixearsm>
2023-05-04 10:01:30 +0000

    r0.0.0.0

---
+++ flake.nix
@@ -0,0 +1,25 @@
+{
+  description = "miscellaneous nix pkgs, grouped in one place";
+
+  inputs = {
+    nixpkgs.url     = github:nixos/nixpkgs/be44bf67; # nixos-22.05 2022-10-15
+    flake-utils.url = github:numtide/flake-utils/c0e246b9;
+    bashHeader      = {
+      url    = github:sixears/bash-header/5206b087;
+      inputs = { nixpkgs.follows = "nixpkgs"; };
+    };
+  };
+
+  outputs = { self, nixpkgs, flake-utils, bashHeader }:
+    flake-utils.lib.eachSystem ["x86_64-linux"] (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+        bash-header = bashHeader.packages.${system}.bash-header;
+      in
+        rec {
+          packages = flake-utils.lib.flattenTree (with pkgs; {
+            touchpad = import ./touchpad.nix { inherit pkgs bash-header; };
+          });
+        }
+    );
+}
