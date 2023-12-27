110210698ac7696874e362e02048ab80187b5f73
Mr. Fluffy <git@sixearsm>
2023-12-27 10:26:09 +0000

    r0.0.1.4

--- byobu.nix
+++ byobu.nix
@@ -8,11 +8,11 @@ esac
 
 # byobu particularly relies on the path to access its own utility programs :-(
 exec                                                                          \
-  env -i USER=$USER HOME=$HOME TERM=$TERM                                     \
+  env -i USER="$USER" HOME="$HOME" TERM="$TERM"                               \
          DBUS_SESSION_BUS_ADDRESS="$DBUS_SESSION_BUS_ADDRESS"                 \
-         XDG_RUNTIME_DIR="$XDG_RUNTIME_DIR"                                   \
-         DISPLAY=$DISPLAY XAUTHORITY=''${XAUTHORITY:-$HOME/.Xauthority}       \
-         PATH=${pkgs.byobu}/bin:${pkgs.tmux}/bin:/run/current-system/sw/bin   \
+         XDG_RUNTIME_DIR="$XDG_RUNTIME_DIR" SWAYSOCK="$SWAYSOCK"              \
+         DISPLAY="$DISPLAY" XAUTHORITY="''${XAUTHORITY:-$HOME/.Xauthority}"   \
+         PATH="${pkgs.byobu}/bin:${pkgs.tmux}/bin:/run/current-system/sw/bin" \
        ${pkgs.byobu}/bin/byobu "$@"
 
 ''
